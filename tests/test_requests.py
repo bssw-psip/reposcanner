144a3485b670798464d4ed0f2782aa20ad924b5c
Reed Milewicz <rmilewi@sandia.gov>
2021-11-08 14:25:47 -0700

    fixed issue with failing test, test_OfflineRoutineRequest_cloneDirectoryIsBasedOnWorkspaceDirectory

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,6 +1,7 @@
 import pytest
 import reposcanner.requests as requests
 from reposcanner.git import CredentialKeychain
+from pathlib import Path
 import platform
 
 
@@ -15,15 +16,15 @@ def test_AnalysisRequestModel_isAnAnalysisRequestType():
 def test_AnalysisRequestModel_hasNoErrorsForValidInput():
         analysisRequest = requests.AnalysisRequestModel(outputDirectory="./")
         assert(not analysisRequest.hasErrors())
-        
+
 def test_AnalysisRequestModel_passingGarbageOutputDirectoryIsAnError():
         analysisRequest = requests.AnalysisRequestModel(outputDirectory=42)
         assert(analysisRequest.hasErrors())
-        
+
 def test_AnalysisRequestModel_passingNonexistentOutputDirectoryIsAnError():
         analysisRequest = requests.AnalysisRequestModel(outputDirectory="./nonexistentDirectory/")
         assert(analysisRequest.hasErrors())
-        
+
 def test_AnalysisRequestModel_passingOutputDirectoryThatCannotBeWrittenToIsAnError():
         #This test is specific to Mac and Linux environments, so we'll skip it when running
 		#this test in a Windows environment.
@@ -41,16 +42,16 @@ def test_AnalysisRequestModel_defaultDataCriteriaAcceptsLiterallyEverything():
 
 def test_RoutineRequestModel_isDirectlyConstructible():
         requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="./")
-        
+
 def test_AnalysisRequestModel_isARoutineRequestType():
         routineRequest = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="./")
         assert(not routineRequest.isAnalysisRequestType())
         assert(routineRequest.isRoutineRequestType())
-        
+
 def test_RoutineRequestModel_hasNoErrorsForValidInput():
         request = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="./")
         assert(not request.hasErrors())
-        
+
 def test_RoutineRequestModel_passingOutputDirectoryThatCannotBeWrittenToIsAnError():
         #This test is specific to Mac and Linux environments, so we'll skip it when running
 		#this test in a Windows environment.
@@ -58,7 +59,7 @@ def test_RoutineRequestModel_passingOutputDirectoryThatCannotBeWrittenToIsAnErro
             return True
         request = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="/")
         assert(request.hasErrors())
-        
+
 def test_RoutineRequestModel_canGenerateAndStoreRepositoryLocation():
         request = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="./")
         location = request.getRepositoryLocation()
@@ -66,33 +67,33 @@ def test_RoutineRequestModel_canGenerateAndStoreRepositoryLocation():
         assert(location.isRecognizable())
         assert(location.getOwner() == "owner")
         assert(location.getRepositoryName() == "repo")
-        
-        
+
+
 def test_RoutineRequestModel_badURLMeansError():
         requestA = requests.RoutineRequestModel(repositoryURL="garbage",outputDirectory="./")
         assert(requestA.hasErrors())
-        
+
         requestB = requests.RoutineRequestModel(repositoryURL="https://unrecognizedhost.org/owner/repo",outputDirectory="./")
         assert(requestB.hasErrors())
-        
+
         requestC = requests.RoutineRequestModel(repositoryURL=None,outputDirectory="./")
         assert(requestC.hasErrors())
 
 def test_RoutineRequestModel_canStoreOutputDirectory():
         request = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="./")
         assert(request.getOutputDirectory() == "./")
-        
+
 def test_RoutineRequestModel_badOutputDirectoryMeansError():
         requestA = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory=None)
         assert(requestA.hasErrors())
-        
+
         requestB = requests.RoutineRequestModel(repositoryURL="https://github.com/owner/repo",outputDirectory="./nonexistent/directory/")
         assert(requestB.hasErrors())
-        
+
 def test_OnlineRoutineRequest_isDirectlyConstructible():
         requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
-                token = "ab5571mc1") 
+                token = "ab5571mc1")
 
 def test_OnlineRoutineRequest_canPassKeychainToConstructor():
         credentialsDictionary = {}
@@ -102,31 +103,31 @@ def test_OnlineRoutineRequest_canPassKeychainToConstructor():
         requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 keychain = keychain)
-        
+
 def test_OnlineRoutineRequest_canStoreValidCredentials():
         requestA = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
-                token = "ab5571mc1") 
+                token = "ab5571mc1")
         assert(not requestA.hasErrors())
         credentialsA = requestA.getCredentials()
         assert(credentialsA.hasTokenAvailable())
         assert(credentialsA.getToken() == "ab5571mc1")
-        
+
         requestB = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 username = "argyle",
-                password = "luggage") 
+                password = "luggage")
         assert(not requestB.hasErrors())
         credentialsB = requestB.getCredentials()
         assert(credentialsB.hasUsernameAndPasswordAvailable())
         assert(credentialsB.getUsername() == "argyle")
         assert(credentialsB.getPassword() == "luggage")
-        
+
 def test_OnlineRoutineRequest_canStoreValidCredentialsViaKeychain():
         credentialsDictionary = {}
         entry = {"url" : "https://github.com/", "token": "ab341m32"}
         credentialsDictionary["platform"] = entry
-        
+
         keychain = CredentialKeychain(credentialsDictionary=credentialsDictionary)
         request = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
@@ -135,12 +136,12 @@ def test_OnlineRoutineRequest_canStoreValidCredentialsViaKeychain():
         credentials = request.getCredentials()
         assert(credentials.hasTokenAvailable())
         assert(credentials.getToken() == "ab341m32")
-         
+
 def test_OnlineRoutineRequest_keychainTakesPrecedenceOverOtherInputs():
         credentialsDictionary = {}
         entry = {"url" : "https://github.com/", "token": "ab341m32"}
         credentialsDictionary["platform"] = entry
-        
+
         keychain = CredentialKeychain(credentialsDictionary=credentialsDictionary)
         request = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
@@ -150,68 +151,63 @@ def test_OnlineRoutineRequest_keychainTakesPrecedenceOverOtherInputs():
         credentials = request.getCredentials()
         assert(credentials.hasTokenAvailable())
         assert(credentials.getToken() == "ab341m32")
-        
+
 def test_OnlineRoutineRequest_doesNotSwitchToOtherCredentialsIfKeychainLacksThem():
         credentialsDictionary = {}
         entry = {"url" : "https://gitlab.com/", "token": "ab341m32"}
         credentialsDictionary["platform"] = entry
-        
+
         keychain = CredentialKeychain(credentialsDictionary=credentialsDictionary)
         request = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 token = "qq4132c",
                 keychain = keychain)
         assert(request.hasErrors())
-        
+
 def test_OnlineRoutineRequest_badCredentialsMeansError():
         requestA = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 token = None)
         assert(requestA.hasErrors())
-        
+
         requestB = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 username = "argyle",
                 password = None)
         assert(requestB.hasErrors())
-        
+
         requestC = requests.OnlineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 username = None,
                 password = "luggage")
         assert(requestC.hasErrors())
-        
+
 def test_OfflineRoutineRequest_isDirectlyConstructible():
         requests.OfflineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 workspaceDirectory="./")
-                
+
 def test_OfflineRoutineRequest_canStoreValidWorkspaceDirectory():
         request = requests.OfflineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 workspaceDirectory="./")
         assert(not request.hasErrors())
         assert(request.getWorkspaceDirectory() == "./")
-        
-        
+
+
 def test_OfflineRoutineRequest_cloneDirectoryIsBasedOnWorkspaceDirectory():
         request = requests.OfflineRoutineRequest(repositoryURL="https://github.com/scikit/scikit-data",
                 outputDirectory="./outputs/",
                 workspaceDirectory="./workspace")
-        assert(request.getCloneDirectory() == "./workspace/scikit-data")
-        
+        assert(request.getCloneDirectory() == Path("./workspace/scikit_scikit-data"))
+
 def test_OfflineRoutineRequest_badWorkspaceDirectoryMeansError():
         requestA = requests.OfflineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 workspaceDirectory = None)
         assert(requestA.hasErrors())
-        
+
         requestB = requests.OfflineRoutineRequest(repositoryURL="https://github.com/owner/repo",
                 outputDirectory="./",
                 workspaceDirectory = "./nonexistent/directory")
         assert(requestB.hasErrors())
-        
-        
-        
-        
-        
